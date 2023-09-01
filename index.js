f56665451b751dd7ff1e8e7bfe52f806b8a1b3a3
Asif Nawaz <asifnawaz.co@gmail.com>
2023-09-01 17:37:28 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-// document.querySelector("button").addEventListener("click", checkWinner);
+document.querySelector("#Player1").innerText = "Player 1";
+document.querySelector("#Number1").innerText = "Player 1";
 
 document.getElementById("run").addEventListener("click", checkWinner);
 
@@ -7,7 +8,7 @@ function checkWinner() {
   var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
   var imageName1 = "./images/dice" + randomNumber1 + ".png";
-  var imageName2 = "images/dice" + randomNumber2 + ".png";
+  var imageName2 = "./images/dice" + randomNumber2 + ".png";
 
   document.querySelector(".img1").setAttribute("src", imageName1);
   document.querySelector(".img2").setAttribute("src", imageName2);
@@ -15,8 +16,28 @@ function checkWinner() {
   if (randomNumber1 > randomNumber2) {
     document.querySelector("h1").innerHTML = "Player 1 Wins! &#128681";
   } else if (randomNumber1 < randomNumber2) {
-    document.querySelector("h1").innerHTML = "Player 2 Wins! &#128681";
+    document.querySelector("h1").innerHTML = "Asif Wins! &#128681";
   } else {
     document.querySelector("h1").innerHTML = "&#128681 Draw! &#128681";
   }
+
+  var table = document.getElementById("table");
+
+  // Create a new table row element
+  var row = document.createElement("tr");
+
+  // Create two table data elements for the numbers
+  var td1 = document.createElement("td");
+  var td2 = document.createElement("td");
+
+  // Set the inner text of the table data elements to the numbers
+  td1.innerText = randomNumber1;
+  td2.innerText = randomNumber2;
+
+  // Append the table data elements to the table row element
+  row.appendChild(td1);
+  row.appendChild(td2);
+
+  // Append the table row element to the table element
+  table.appendChild(row);
 }
